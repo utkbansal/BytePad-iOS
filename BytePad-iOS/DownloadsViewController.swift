16cf13ecd00bea9c1c1def76fad84debe7ac58bb
Bansal Utkarsh <bansalutkarsh3@gmail.com>
2016-11-13 02:23:12 +0530

    Adds downloads table view

---
+++ BytePad-iOS/DownloadsViewController.swift
@@ -0,0 +1,52 @@
+//
+//  DownloadsViewController.swift
+//  BytePad-iOS
+//
+//  Created by Utkarsh Bansal on 13/11/16.
+//  Copyright © 2016 Software Incubator. All rights reserved.
+//
+
+import UIKit
+
+class DownloadsViewController: UIViewController {
+
+    @IBOutlet weak var downloadsTableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+        
+        self.downloadsTableView.dataSource = self
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension DownloadsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.downloadsTableView.dequeueReusableCell(withIdentifier: "downloads-cell")
+        cell?.textLabel?.text = "fuck"
+        
+        return cell!
+        
+    }
+}
