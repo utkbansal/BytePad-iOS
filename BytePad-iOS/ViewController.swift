4add6a7a8d30e1e40fc3274be2c6b4f56139e7d5
Bansal Utkarsh <bansalutkarsh3@gmail.com>
2016-11-13 00:38:19 +0530

    Initial Commit

---
+++ BytePad-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BytePad-iOS
+//
+//  Created by Utkarsh Bansal on 13/11/16.
+//  Copyright © 2016 Software Incubator. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ BytePad-iOSUITests/BytePad_iOSUITests.swift
@@ -0,0 +1,36 @@
+//
+//  BytePad_iOSUITests.swift
+//  BytePad-iOSUITests
+//
+//  Created by Utkarsh Bansal on 13/11/16.
+//  Copyright © 2016 Software Incubator. All rights reserved.
+//
+
+import XCTest
+
+class BytePad_iOSUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
