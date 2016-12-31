70fae382a3d30bd30013b1f0d7a5b2b95578304b
Bansal Utkarsh <bansalutkarsh3@gmail.com>
2016-12-31 14:57:17 +0530

    Shows downloaded files in the downloads tab

--- BytePad-iOS/DownloadsViewController.swift
+++ BytePad-iOS/DownloadsViewController.swift
@@ -11,6 +11,34 @@ import UIKit
 class DownloadsViewController: UIViewController {
 
     @IBOutlet weak var downloadsTableView: UITableView!
+    
+    var downloadedPapers: [(name: String, url: String)] = []
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let documentsUrl =  FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        
+        // now lets get the directory contents (including folders)
+        do {
+            let directoryContents = try FileManager.default.contentsOfDirectory(at: documentsUrl, includingPropertiesForKeys: nil, options: FileManager.DirectoryEnumerationOptions())
+            print(directoryContents)
+            
+            for  file in directoryContents {
+                print(file.lastPathComponent)
+                print(file.absoluteURL)
+                print(file.baseURL)
+                print((file as NSURL).filePathURL)
+                
+                // Save the data in the list as a tuple
+                self.downloadedPapers.append((file.lastPathComponent, file.absoluteString))
+            }
+            
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -30,12 +58,12 @@ class DownloadsViewController: UIViewController {
 
 extension DownloadsViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 20
+        return self.downloadedPapers.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = self.downloadsTableView.dequeueReusableCell(withIdentifier: "downloads-cell")
-        cell?.textLabel?.text = "Downloaded file"
+        cell?.textLabel?.text = self.downloadedPapers[indexPath.row].name
         
         return cell!
         
